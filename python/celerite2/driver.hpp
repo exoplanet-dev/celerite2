2d1fdcbf6e821e8bebbc7ede9ea55c13c88a45bc
Dan F-M <foreman.mackey@gmail.com>
2020-07-13 12:52:11 -0400

    one more try

--- python/celerite2/driver.hpp
+++ python/celerite2/driver.hpp
@@ -22,6 +22,22 @@ struct linalg_exception : public std::exception {
 
 // Some pre-processor magic to get faster runtimes for small systems
 
+#define UNWRAP_CASES_FEW                                                                                                                             \
+  switch (J) {                                                                                                                                       \
+    case 1: FIXED_SIZE_MAP(1); break;                                                                                                                \
+    case 2: FIXED_SIZE_MAP(2); break;                                                                                                                \
+    case 3: FIXED_SIZE_MAP(3); break;                                                                                                                \
+    case 4: FIXED_SIZE_MAP(4); break;                                                                                                                \
+    case 5: FIXED_SIZE_MAP(5); break;                                                                                                                \
+    case 6: FIXED_SIZE_MAP(6); break;                                                                                                                \
+    case 7: FIXED_SIZE_MAP(7); break;                                                                                                                \
+    case 8: FIXED_SIZE_MAP(8); break;                                                                                                                \
+    case 9: FIXED_SIZE_MAP(9); break;                                                                                                                \
+    default: FIXED_SIZE_MAP(Eigen::Dynamic);                                                                                                         \
+  }
+
+#ifdef CELERITE2_FAST_RUN
+
 #define UNWRAP_CASES                                                                                                                                 \
   switch (J) {                                                                                                                                       \
     case 1: FIXED_SIZE_MAP(1); break;                                                                                                                \
@@ -59,76 +75,11 @@ struct linalg_exception : public std::exception {
     default: FIXED_SIZE_MAP(Eigen::Dynamic);                                                                                                         \
   }
 
-#define UNWRAP_CASES_FEW                                                                                                                             \
-  switch (J) {                                                                                                                                       \
-    case 1: FIXED_SIZE_MAP(1); break;                                                                                                                \
-    case 2: FIXED_SIZE_MAP(2); break;                                                                                                                \
-    case 3: FIXED_SIZE_MAP(3); break;                                                                                                                \
-    case 4: FIXED_SIZE_MAP(4); break;                                                                                                                \
-    case 5: FIXED_SIZE_MAP(5); break;                                                                                                                \
-    case 6: FIXED_SIZE_MAP(6); break;                                                                                                                \
-    case 7: FIXED_SIZE_MAP(7); break;                                                                                                                \
-    case 8: FIXED_SIZE_MAP(8); break;                                                                                                                \
-    case 9: FIXED_SIZE_MAP(9); break;                                                                                                                \
-    default: FIXED_SIZE_MAP(Eigen::Dynamic);                                                                                                         \
-  }
+#else
 
-// #ifdef CELERITE2_FAST_RUN
+#define UNWRAP_CASES UNWRAP_CASES_FEW
 
-// #define UNWRAP_CASES                                                                                                                                 \
-//   switch (J) {                                                                                                                                       \
-//     case 1: FIXED_SIZE_MAP(1); break;                                                                                                                \
-//     case 2: FIXED_SIZE_MAP(2); break;                                                                                                                \
-//     case 3: FIXED_SIZE_MAP(3); break;                                                                                                                \
-//     case 4: FIXED_SIZE_MAP(4); break;                                                                                                                \
-//     case 5: FIXED_SIZE_MAP(5); break;                                                                                                                \
-//     case 6: FIXED_SIZE_MAP(6); break;                                                                                                                \
-//     case 7: FIXED_SIZE_MAP(7); break;                                                                                                                \
-//     case 8: FIXED_SIZE_MAP(8); break;                                                                                                                \
-//     case 9: FIXED_SIZE_MAP(9); break;                                                                                                                \
-//     case 10: FIXED_SIZE_MAP(10); break;                                                                                                              \
-//     case 11: FIXED_SIZE_MAP(11); break;                                                                                                              \
-//     case 12: FIXED_SIZE_MAP(12); break;                                                                                                              \
-//     case 13: FIXED_SIZE_MAP(13); break;                                                                                                              \
-//     case 14: FIXED_SIZE_MAP(14); break;                                                                                                              \
-//     case 15: FIXED_SIZE_MAP(15); break;                                                                                                              \
-//     case 16: FIXED_SIZE_MAP(16); break;                                                                                                              \
-//     case 17: FIXED_SIZE_MAP(17); break;                                                                                                              \
-//     case 18: FIXED_SIZE_MAP(18); break;                                                                                                              \
-//     case 19: FIXED_SIZE_MAP(19); break;                                                                                                              \
-//     case 20: FIXED_SIZE_MAP(20); break;                                                                                                              \
-//     case 21: FIXED_SIZE_MAP(21); break;                                                                                                              \
-//     case 22: FIXED_SIZE_MAP(22); break;                                                                                                              \
-//     case 23: FIXED_SIZE_MAP(23); break;                                                                                                              \
-//     case 24: FIXED_SIZE_MAP(24); break;                                                                                                              \
-//     case 25: FIXED_SIZE_MAP(25); break;                                                                                                              \
-//     case 26: FIXED_SIZE_MAP(26); break;                                                                                                              \
-//     case 27: FIXED_SIZE_MAP(27); break;                                                                                                              \
-//     case 28: FIXED_SIZE_MAP(28); break;                                                                                                              \
-//     case 29: FIXED_SIZE_MAP(29); break;                                                                                                              \
-//     case 30: FIXED_SIZE_MAP(30); break;                                                                                                              \
-//     case 31: FIXED_SIZE_MAP(31); break;                                                                                                              \
-//     case 32: FIXED_SIZE_MAP(32); break;                                                                                                              \
-//     default: FIXED_SIZE_MAP(Eigen::Dynamic);                                                                                                         \
-//   }
-
-// #else
-
-// #define UNWRAP_CASES                                                                                                                                 \
-//   switch (J) {                                                                                                                                       \
-//     case 1: FIXED_SIZE_MAP(1); break;                                                                                                                \
-//     case 2: FIXED_SIZE_MAP(2); break;                                                                                                                \
-//     case 3: FIXED_SIZE_MAP(3); break;                                                                                                                \
-//     case 4: FIXED_SIZE_MAP(4); break;                                                                                                                \
-//     case 5: FIXED_SIZE_MAP(5); break;                                                                                                                \
-//     case 6: FIXED_SIZE_MAP(6); break;                                                                                                                \
-//     case 7: FIXED_SIZE_MAP(7); break;                                                                                                                \
-//     case 8: FIXED_SIZE_MAP(8); break;                                                                                                                \
-//     case 9: FIXED_SIZE_MAP(9); break;                                                                                                                \
-//     default: FIXED_SIZE_MAP(Eigen::Dynamic);                                                                                                         \
-//   }
-
-// #endif
+#endif
 
 // These are some generally useful macros for interfacing between numpy and Eigen
 template <int Size>
