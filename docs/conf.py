953d6c5673b64aee86f877b09f2eef988dfa24c1
Dan F-M <foreman.mackey@gmail.com>
2020-07-20 16:26:03 -0400

    start building docs

---
+++ docs/conf.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sphinx_material
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution("celerite2").version
+except DistributionNotFound:
+    __version__ = "unknown version"
+
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "sphinx_copybutton",
+]
+
+autodoc_mock_imports = [
+    "numpy",
+    "theano",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
+}
+
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "index"
+
+# General information about the project.
+project = "celerite2"
+author = "Dan Foreman-Mackey"
+copyright = "2020, " + author
+
+version = __version__
+release = __version__
+
+exclude_patterns = ["_build"]
+
+# HTML theme
+html_show_sourcelink = False
+html_sidebars = {
+    "**": [
+        "logo-text.html",
+        "globaltoc.html",
+        "localtoc.html",
+        "searchbox.html",
+    ]
+}
+
+extensions.append("sphinx_material")
+html_theme_path = sphinx_material.html_theme_path()
+html_context = sphinx_material.get_html_context()
+html_theme = "sphinx_material"
+html_title = html_short_title = "celerite2"
+
+html_favicon = "_static/logo.png"
+html_logo = "_static/logo-inv.png"
+html_theme_options = {
+    "nav_title": "celerite2",
+    # "base_url": "https://project.github.io/project",
+    "color_primary": "blue",
+    "color_accent": "light-blue",
+    "repo_url": "https://github.com/dfm/celerite2",
+    "repo_name": "celerite2",
+    "globaltoc_depth": 1,
+    "globaltoc_collapse": False,
+    "globaltoc_includehidden": False,
+    "heroes": {"index": "Fast & scalable Gaussian Processes in one dimension"},
+    "nav_links": [],
+}
---
+++ docs/index.rst
@@ -0,0 +1,70 @@
+=========
+celerite2
+=========
+
+It's real fast.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: User Guide
+
+   install
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Tutorials
+
+   install
+
+License & attribution
+---------------------
+
+Copyright 2020 Daniel Foreman-Mackey.
+
+The source code is made available under the terms of the MIT license.
+
+If you make use of this code, please cite the relevant papers with the following
+BibTeX:
+
+.. code-block:: tex
+
+    @article{celerite1,
+      author = {{Foreman-Mackey}, D. and {Agol}, E. and {Ambikasaran}, S. and
+                {Angus}, R.},
+        title = "{Fast and Scalable Gaussian Process Modeling with Applications to
+                  Astronomical Time Series}",
+      journal = {\aj},
+        year = 2017,
+        month = dec,
+      volume = 154,
+        pages = {220},
+          doi = {10.3847/1538-3881/aa9332},
+      adsurl = {http://adsabs.harvard.edu/abs/2017AJ....154..220F},
+      adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+    }
+    @article{celerite2,
+      author = {{Foreman-Mackey}, D.},
+        title = "{Scalable Backpropagation for Gaussian Processes using Celerite}",
+      journal = {Research Notes of the American Astronomical Society},
+        year = 2018,
+        month = feb,
+      volume = 2,
+      number = 1,
+        pages = {31},
+          doi = {10.3847/2515-5172/aaaf6c},
+      adsurl = {http://adsabs.harvard.edu/abs/2018RNAAS...2a..31F},
+      adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+    }
+
+These docs were made using `Sphinx <https://www.sphinx-doc.org>`_ and the
+`sphinx-material theme <bashtage/sphinx-material>`_. They are built and hosted
+on `Read the Docs <https://readthedocs.org>`_.
+
+Icons made by `monkik <https://www.flaticon.com/authors/monkik>`_ from
+`flaticon.com <https://www.flaticon.com>`_.
+
+
+Changelog
+---------
+
+.. include:: ../HISTORY.rst
