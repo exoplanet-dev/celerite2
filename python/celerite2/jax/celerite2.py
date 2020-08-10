929f99060fa38f90a7c91444b45a02f1a4429667
Dan F-M <foreman.mackey@gmail.com>
2020-08-10 14:10:00 -0400

    simplifying jax implementation

--- python/celerite2/jax/celerite2.py
+++ python/celerite2/jax/celerite2.py
@@ -4,63 +4,21 @@ __all__ = ["GaussianProcess"]
 from jax import numpy as np
 
 from .. import backprop, driver
-from ..celerite2 import ConstantMean
-from ..celerite2 import GaussianProcess as SuperGaussianProcess
+from ..ext import BaseGaussianProcess
 from . import ops
 
 
-class GaussianProcess(SuperGaussianProcess):
-    def __init__(self, kernel, t=None, *, mean=0.0, **kwargs):
-        self._kernel = kernel
-        if callable(mean):
-            self._mean = mean
-        else:
-            self._mean = ConstantMean(mean)
+class GaussianProcess(BaseGaussianProcess):
+    def as_tensor(self, tensor):
+        return np.asarray(tensor, dtype=np.float64)
 
-        # Placeholders for storing data
-        self._t = None
-        self._diag = None
-        self._log_det = -np.inf
-        self._norm = np.inf
+    def zeros_like(self, tensor):
+        return np.zeros_like(tensor)
 
-        if t is not None:
-            self.compute(t, **kwargs)
-
-    def compute(
-        self, t, *, yerr=None, diag=None, check_sorted=True, quiet=False
-    ):
-        # Check the input coordinates
-        t = np.atleast_1d(t)
-        if check_sorted and np.any(np.diff(t) < 0.0):
-            raise ValueError("the input coordinates must be sorted")
-        if len(t.shape) != 1:
-            raise ValueError("the input coordinates must be one dimensional")
-
-        # Save the diagonal
-        self._t = np.asarray(t, dtype=np.float64)
-        self._mean_value = self._mean(self._t)
-        self._diag = np.zeros_like(self._t)
-        if yerr is None and diag is None:
-            pass
-
-        elif yerr is not None:
-            if diag is not None:
-                raise ValueError(
-                    "only one of 'diag' and 'yerr' can be provided"
-                )
-            self._diag += np.atleast_1d(yerr) ** 2
-
-        else:
-            self._diag += np.atleast_1d(diag)
-
-        # Fill the celerite matrices
-        a, self._U, self._V, self._P = self._kernel.get_celerite_matrices(
-            self._t, self._diag
-        )
-
+    def do_compute(self, quiet):
         # Compute the Cholesky factorization
         try:
-            self._d, self._W = ops.factor(a, self._U, self._V, self._P)
+            self._d, self._W = ops.factor(self._a, self._U, self._V, self._P)
         except driver.LinAlgError:
             if not quiet:
                 raise backprop.LinAlgError(
@@ -74,98 +32,25 @@ class GaussianProcess(SuperGaussianProcess):
                 self._log_det + len(self._t) * np.log(2 * np.pi)
             )
 
-    def recompute(self, *, quiet=False):
-        if self._t is None:
-            raise RuntimeError(
-                "you must call 'compute' directly  at least once"
-            )
-        return self.compute(
-            self._t, diag=self._diag, check_sorted=False, quiet=quiet
-        )
+    def check_sorted(self, t):
+        if np.any(np.diff(t) < 0.0):
+            raise ValueError("the input coordinates must be sorted")
+        return t
 
-    def _process_input(self, y, *, require_vector=False):
-        y = np.atleast_1d(y)
-        if self._t is None:
-            raise RuntimeError("you must call 'compute' first")
-        if self._t.shape[0] != y.shape[0]:
-            raise ValueError("dimension mismatch")
-        if require_vector and self._t.shape != y.shape:
-            raise ValueError("'y' must be one dimensional")
-        y = np.array(y, dtype=np.float64, copy=True)
-        return y
-
-    def apply_inverse(self, y):
-        y = self._process_input(y)
+    def do_solve(self, y):
         return ops.solve(self._U, self._P, self._d, self._W, y)
 
-    def dot_tril(self, y):
-        y = self._process_input(y)
+    def do_dot_tril(self, y):
         return ops.dot_tril(self._U, self._P, self._d, self._W, y)
 
-    def log_likelihood(self, y):
-        y = self._process_input(y, require_vector=True)
-        if not np.isfinite(self._log_det):
-            return -np.inf
-        loglike = self._norm - 0.5 * ops.norm(
-            self._U, self._P, self._d, self._W, y - self._mean_value
-        )
-        if not np.isfinite(loglike):
-            return -np.inf
-        return loglike
+    def do_norm(self, y):
+        return ops.norm(self._U, self._P, self._d, self._W, y)
 
-    def predict(
-        self,
-        y,
-        t=None,
-        *,
-        return_cov=False,
-        return_var=False,
-        include_mean=True,
-        kernel=None,
-    ):
-        y = self._process_input(y, require_vector=True)
+    def do_conditional_mean(self, *args):
+        return ops.conditional_mean(self._U, self._V, self._P, *args)
 
-        resid = y - self._mean(self._t)
-        alpha = ops.solve(self._U, self._P, self._d, self._W, resid)
+    def tensordot(self, a, b):
+        return np.tensordot(a, b, axes=(0, 0))
 
-        if t is None:
-            xs = self._t
-            mu = y - self._diag * alpha
-
-        else:
-            xs = np.asarray(t, dtype=np.float64)
-            if xs.ndim != 1:
-                raise ValueError("dimension mismatch")
-
-            U_star, V_star, inds = self._kernel.get_conditional_mean_matrices(
-                self._t, xs
-            )
-            mu = ops.conditional_mean(
-                self._U, self._V, self._P, alpha, U_star, V_star, inds
-            )
-            if include_mean:
-                mu += self._mean(self._t)
-
-        if not (return_var or return_cov):
-            return mu
-
-        # Predictive variance.
-        KxsT = self._kernel.get_value(xs[None, :] - self._t[:, None])
-        if return_var:
-            var = self._kernel.get_value(0.0) - np.einsum(
-                "ij,ij->j", KxsT, self.apply_inverse(KxsT)
-            )
-            return mu, var
-
-        # Predictive covariance
-        cov = self._kernel.get_value(xs[:, None] - xs[None, :])
-        cov -= np.tensordot(KxsT, self.apply_inverse(KxsT), axes=(0, 0))
-        return mu, cov
-
-    def sample(self, *args, **kwargs):
-        raise NotImplementedError("'sample' is not implemented in jax")
-
-    def sample_conditional(self, *args, **kwargs):
-        raise NotImplementedError(
-            "'sample_conditional' is not implemented in jax"
-        )
+    def diagdot(self, a, b):
+        return np.einsum("ij,ij->j", a, b)
